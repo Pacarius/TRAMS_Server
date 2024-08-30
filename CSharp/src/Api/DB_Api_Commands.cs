7868fc13de42415dff9294e2f3fdbe1a0cd31051
Pacarius <pcpgsk@gmail.com>
2024-08-30 16:32:01 +0800

    Finished up APIs and refactored router to use c# attributes to lower the future pains of different routes. But multitargetting classes needs like a base class or an interface, I might get around to that eventually, but the server should be around 90% for prototyping.

--- CSharp/src/Api/DB_Api_Commands.cs
+++ CSharp/src/Api/DB_Api_Commands.cs
@@ -1,81 +1,56 @@
-using System.ComponentModel.Design.Serialization;
+
 using System.Text.Json;
 using CSharp.src.Generics;
 using Microsoft.AspNetCore.Http;
 using MySqlConnector;
 
-namespace DB{
-    internal partial class DBCommands{
+namespace DB
+{
+    internal partial class DBCommands
+    {
         static ILoggingHelper helper = new() { LoggingDepth = 2 };
         static DBConnector Connector = Program.dBConnector;
         static readonly string GetEventsString = "";
-        static readonly string GetRString_Owner = "SELECT * FROM `CheckReservations` WHERE `OwnerName` = @OwnerName";
-        static readonly string GetRString_Street = "SELECT `Beg_Datetime`, `Duration`, `X`, `Y`, `Status` FROM `CheckReservations` WHERE `street_NAME` = @Street";
-        static readonly string GetVInfoString = "SELECT `ID`, `Type`, `OwnerName` FROM `vehicle` WHERE `ID` = @PlateNo";
+        static readonly string GetRString_Owner = "SELECT * FROM `CheckReservations` WHERE `OwnerName` = OwnerName";
+        static readonly string GetRString_Street = "SELECT `Beg_Datetime`, `Duration`, `X`, `Y`, `Status` FROM `CheckReservations` WHERE `street_NAME` = Street";
+        static readonly string GetVInfoString = "SELECT `ID`, `Type`, `OwnerName` FROM `vehicle` WHERE `ID` = PlateNo";
         static readonly string GetViolationsString = "";
-        static readonly string InsertReservationString = "INSERT INTO `reservation`(`Beg_Datetime`,`Duration`,`X`,`Y`,`street_NAME`,`vehicle_ID`) VALUES(@Datetime, @Duration, X, Y, @Street, @VehicleID)";
+        static readonly string InsertReservationString = "INSERT INTO `reservation`(`Beg_Datetime`,`Duration`,`X`,`Y`,`street_NAME`,`vehicle_ID`) VALUES(Datetime, Duration, X, Y, Street, VehicleID)";
         static readonly string VRecentRecordsString = "";
         //Helper method for formatting queries to return raw JSONS.
-        static async Task<string> QToJson(string Query, Dictionary<string, string> Params){
-            MySqlCommand cmd = new()
+        static async Task<string> QToJson(string Query, Dictionary<string, string> Params)
+        {
+            try
             {
-                CommandText = Query,
-                Connection = await Connector.GetConnection()
-            };
-            foreach(var param in Params)
-                cmd.Parameters.Add(new() { ParameterName = param.Key, Value = param.Value });
-            using var r = await cmd.ExecuteReaderAsync();
-            var results = new List<Dictionary<string, object>>();
-            while (await r.ReadAsync())
-            {
-                var row = new Dictionary<string, object>();
-                for (int i = 0; i < r.FieldCount; i++)
+                MySqlCommand cmd = new(Query, await Connector.GetConnection());
+                _ = Params.Select(c => cmd.Parameters.AddWithValue(c.Key, c.Value));
+                using var r = await cmd.ExecuteReaderAsync();
+                var results = new List<Dictionary<string, object>>();
+                while (await r.ReadAsync())
                 {
-                    row[r.GetName(i)] = r.GetValue(i);
+                    var row = Enumerable.Range(0, r.FieldCount)
+                        .ToDictionary(r.GetName, r.GetValue);
+                    results.Add(row);
                 }
-                results.Add(row);
+                return JsonSerializer.Serialize(results);
             }
-            return JsonSerializer.Serialize(results);
+            catch (MySqlException e) { helper.Log(e.Message, "DBCommands"); return e.Message; }
         }
-        public static RequestDelegate getRStringOwner = async context => {
-            string Owner = context.Request.Query["Owner"];
-            string res = await QToJson(GetRString_Owner, new() { { "@OwnerName", Owner } });
+        static async Task RequestHandler(string Query, List<(string HttpName, string ParameterName)> values, HttpContext context)
+        {
+            var Params = values.ToDictionary(c => c.HttpName, c => context.Request.Query[c.HttpName].ToString());
+            var Res = await QToJson(Query, Params);
             context.Response.ContentType = "application/json";
-            await context.Response.WriteAsync(res);
-            helper.Log($"Owner: {res}", "DBCommands");
-        };
-        public static RequestDelegate getRStringStreet = async context => {
-            string Street = context.Request.Query["Street"];
-            string res = await QToJson(GetRString_Street, new() { { "@Street", Street } });
-            context.Response.ContentType = "application/json";
-            await context.Response.WriteAsync(res);
-            helper.Log($"Street: {res}", "DBCommands");
-        };
-        public static RequestDelegate getVInfo = async context => {
-            string PlateNo = context.Request.Query["PlateNo"];
-            string res = await QToJson(GetVInfoString, new() { { "@PlateNo", PlateNo } });
-            context.Response.ContentType = "application/json";
-            await context.Response.WriteAsync(res);
-            helper.Log($"Vehicle: {res}", "DBCommands");
-        };
-        public static RequestDelegate insertReservation = async context => {
-            Dictionary<string, string> Params = new(){
-                {"Datetime", context.Request.Query["Datetime"]},
-                {"Duration", context.Request.Query["Duration"]},
-                {"X", context.Request.Query["X"]},
-                {"Y", context.Request.Query["Y"]},
-                {"Street", context.Request.Query["Street"]},
-                {"VehicleID", context.Request.Query["VehicleID"]}
-            };
-            MySqlCommand cmd = new()
-            {
-                CommandText = InsertReservationString,
-                Connection = await Connector.GetConnection()
-            };
-            foreach(var param in Params)
-                cmd.Parameters.Add(new() { ParameterName = param.Key, Value = param.Value });
-                try{await cmd.ExecuteNonQueryAsync();}catch(Exception e){helper.Log(e.Message, "DBCommands");context.Response.StatusCode = 500;}
-            helper.Log($"Reservation: {Params["Datetime"]}, {Params["Duration"]}, {Params["X"]}, {Params["Y"]}, {Params["Street"]}, {Params["VehicleID"]}", "DBCommands");
-        };
+            helper.Log($"Owner: {Res}", "DBCommands");
+            await context.Response.WriteAsync(Res);
+        }
+        [Api.Routes("getrowner")]
+        public static async Task GetRSOwner(HttpContext context) => await RequestHandler(GetRString_Owner, [("owner", "@OwnerName")], context);
+        [Api.Routes("getrstreet")]
+        public static async Task GetRStreet(HttpContext context) => await RequestHandler(GetRString_Street, [("street", "@Street")], context);
+        [Api.Routes("getvinfo")]
+        public static async Task GetVInfo(HttpContext context) => await RequestHandler(GetVInfoString, [("plateNo", "@PlateNo")], context);
+        [Api.Routes("insertreservation")]
+        public static async Task InsertReservation(HttpContext context) => await RequestHandler(InsertReservationString, [("datetime", "@Datetime"), ("Duration", "@Duration"), ("X", "@X"), ("Y", "@Y"), ("Street", "@Street"), ("VehicleID", "@VehicleID")], context);
     }
 }
--- CSharp/src/Api/Routes.cs
+++ CSharp/src/Api/Routes.cs
@@ -1,17 +1,35 @@
-using System.ComponentModel.Design.Serialization;
-using DB;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
 
-namespace Api{
-    public class Routes{
-        //Routes don't need leading slash(/)
-        public static readonly Dictionary<string, RequestDelegate> RouteList = new(){
-            {"A", async context => {await context.Response.WriteAsync("A");}},
-            {"getrstring_owner", DBCommands.getRStringOwner},
-            {"getrstring_street", DBCommands.getRStringStreet},
-            {"getvinfo", DBCommands.getVInfo},
-            {"insertreservation", DBCommands.insertReservation},
-            };
+namespace Api
+{
+        public class Routes
+    {
+        // Routes don't need leading slash(/)
+        public static readonly Dictionary<string, RequestDelegate> RouteList = [];
+
+        static Routes()
+        {
+            var methods = typeof(DB.DBCommands).GetMethods()
+                .Where(m => m.GetParameters().Length == 1 && m.GetParameters()[0].ParameterType == typeof(HttpContext) && m.ReturnType == typeof(Task));
+
+            RouteList = methods
+                .Select(method => new { Method = method, Attribute = method.GetCustomAttribute<RoutesAttribute>() })
+                .Where(x => x.Attribute != null)
+                .ToDictionary(x => x.Attribute.RouteName, x => CreateDelegate(x.Method));
+        }
+        private static RequestDelegate CreateDelegate(MethodInfo method)=>
+            async context => await (Task)method.Invoke(Activator.CreateInstance(method.DeclaringType), [context]);
+        
+    }
+
+    [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
+    sealed class RoutesAttribute(string routeName) : Attribute
+    {
+        public string RouteName { get; } = routeName;
     }
 }
